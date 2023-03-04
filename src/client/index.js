de728552f88d499ec4b49a61e4ae1c24fea40fb1
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-03-03 17:12:15 -0800

    can get postman but no console logs

--- src/client/index.js
+++ src/client/index.js
@@ -6,8 +6,3 @@ import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
-
-console.log(checkForName);
-
-alert("I EXIST");
-console.log("CHANGE!!");
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,53 @@
-var path = require("path");
+const path = require("path");
 const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const apiKey = process.env.API_KEY;
+const apiUrl = "https://api.meaningcloud.com/sentiment-2.1";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.text());
+app.use(bodyParser.json());
 
 app.use(express.static("dist"));
 
 console.log(__dirname);
 
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
 });
 
-// designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
 });
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
-});
+const formdata = new FormData();
+
+formdata.append("key", apiKey);
+formdata.append("txt", "Hello");
+formdata.append("lang", "en");
+
+const requestOptions = {
+  method: "POST",
+  body: formdata,
+};
+
+const response = fetch(
+  `https://api.meaningcloud.com/sentiment-2.1?&key=${process.env.API_KEY}&lang=en&txt=hello`,
+  requestOptions
+)
+  .then((response) => ({
+    status: response.status,
+    body: response.json(),
+  }))
+  .then(({ status, body }) => console.log(status, body))
+  .catch((error) => console.log("error", error));
+
+console.log(response);
