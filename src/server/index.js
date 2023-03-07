5898d564c618937f58ffa949179763ba1f75f2aa
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-03-06 23:52:49 -0800

    Cleaning up codebase

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,10 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const FormData = require("form-data");
 const fetch = require("node-fetch");
 const mockAPIResponse = require("./mockAPI.js");
 
-const dotenv = require("dotenv");
-dotenv.config();
-
-const apiKey = process.env.API_KEY;
-// const apiUrl = "https://api.meaningcloud.com/sentiment-2.1";
+require("dotenv").config({ path: "./.env" });
 
 const app = express();
 app.use(cors());
@@ -19,41 +14,35 @@ app.use(bodyParser.json());
 
 app.use(express.static("dist"));
 
+const apiKey = process.env.API_KEY;
+console.log(`Your api key is ${apiKey}`); // Used for testing API key entry
+
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
 });
 
-app.listen(8080, function () {
-  console.log("Example app listening on port 8080!");
+app.listen(8081, function () {
+  console.log("Example app listening on port 8081!");
 });
 
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
 
-app.post("/data", (req, res) => {
-  const formdata = new FormData();
+app.post("/postData", async (req, res) => {
+  console.log("Getting Data");
 
-  formdata.append("key", apiKey);
-  formdata.append("txt", req.body);
-  formdata.append("lang", "en");
+  let formText = req.body.url;
+  const baseURL = "https://api.meaningcloud.com/sentiment-2.1?";
 
-  const requestOptions = {
+  const result = await fetch(`${baseURL}key=${apiKey}&lang=en&url=${formText}`, {
     method: "POST",
-    credential: "same-origin",
-    body: formdata,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+  });
+  try {
+    const data = await result.json();
 
-  const response = fetch(`https://api.meaningcloud.com/sentiment-2.1`, requestOptions)
-    .then((response) => ({
-      status: response.status,
-      body: response.json(),
-    }))
-    .then(({ status, body }) => console.log(status, body))
-    .catch((error) => console.log("error", error));
-
-  return response;
+    res.send(data);
+  } catch (error) {
+    console.log("error", error);
+  }
 });
